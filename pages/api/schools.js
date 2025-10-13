a50d391be26e9ee89e0dc17411d07a06408a1b59
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-10-13 17:22:27 +0000

    update error message
    update message

--- pages/api/schools.js
+++ pages/api/schools.js
@@ -6,6 +6,11 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       const data = await prisma.school.findMany();
+      res.status(200).json({
+        success: true,
+        message: "All School fetched successfully",
+        data,
+      });
       res
         .status(200)
         .json({ success: true, message: "School fetch successful", data });
@@ -20,7 +25,7 @@ export default async function handler(req, res) {
     if (!session.user) {
       return res
         .status(401)
-        .json({ success: false, message: "Unauthorize user " });
+        .json({ success: false, message: "Unauthorize User" });
     }
     try {
       const data = req.body;
@@ -38,9 +43,7 @@ export default async function handler(req, res) {
       });
     } catch (error) {
       console.error("Failed to add school:", error);
-      res
-        .status(500)
-        .json({ success: false, message: "Error during add school" });
+      res.status(500).json({ success: false, message: "Failed to add School" });
     }
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
