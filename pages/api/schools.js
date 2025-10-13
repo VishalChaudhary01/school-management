77a65ca0bde165de8e6d628ec60ba7d87e74a0ed
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-10-13 17:23:10 +0000

    udpate success message

--- pages/api/schools.js
+++ pages/api/schools.js
@@ -13,7 +13,11 @@ export default async function handler(req, res) {
       });
       res
         .status(200)
-        .json({ success: true, message: "School fetch successful", data });
+        .json({
+          success: true,
+          message: "All School fetched successfully",
+          data,
+        });
     } catch (error) {
       console.error("Failed to fetch schools", error);
       res
